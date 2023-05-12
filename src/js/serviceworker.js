bda8b953d44e52a3ea11ab713908d649239b5080
Niko Steiner <80452066+Lun4-R@users.noreply.github.com>
2023-05-13 01:47:59 +0300

    Add files via upload

---
+++ src/js/serviceworker.js
@@ -0,0 +1,14 @@
+const cacheName = "The-Stellar-Tree";
+
+self.addEventListener("fetch", event => {
+    event.respondWith((async () => {
+        try {
+            const res = await fetch(event.request);
+            (await caches.open(cacheName)).put(event.request, res.clone());
+            return res;
+        }
+        catch (err) {
+            return await caches.match(event.request);
+        }
+    })());
+});
