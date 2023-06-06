a1fe4e63c0660cb17bac1f91ae9517a4116af945
z3n1th4l@gmail.com <z3n1th4l@gmail.com>
2023-06-06 18:26:31 +0300

    ...

--- js/pwa.js
+++ js/pwa.js
@@ -1,10 +1,9 @@
 const btnInstall = document.querySelector("button#install_app");
-const appInfo = document.querySelector("div#install_app_info");
 
 function addPwaInstall() {
     window.addEventListener("beforeinstallprompt", e => {
         btnInstall.style.display = "block";
-        appInfo.style.display = "block";
+
         btnInstall.addEventListener("click", clickEvent => {
             e.prompt();
             e.userChoice.then(choice => {
